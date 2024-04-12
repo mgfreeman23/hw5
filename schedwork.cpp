92b14d08adaeec139e2f4655af233f7d7c56dfff
Malina Freeman <mgfreema@usc.edu>
2024-04-12 03:51:57 +0000

    second commit

--- schedwork.cpp
+++ schedwork.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <unordered_map>
 #include <algorithm>
 // add or remove necessary headers as you please
 
@@ -21,8 +22,17 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool isValid(int row, int schedCol, const size_t maxShifts, DailySchedule& sched, const size_t dailyNeed, unordered_map<int,int>& worker_shifts, unordered_map<int,int>& day_shifts);
 
-
+bool scheduleHelper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    int rowIdx,
+    int colIdx,
+     unordered_map<int,int>& worker_shifts,
+     unordered_map<int,int>& day_shifts);
 // Add your implementation of schedule() and other helper functions here
 
 bool schedule(
@@ -36,10 +46,107 @@ bool schedule(
         return false;
     }
     sched.clear();
+    sched.resize(avail.size());
+    // map between worker ids and the number of shifts they get are scheduled
+   unordered_map<int, int> worker_shifts;
+   int numWorkers = avail[0].size();
+   for(int i = 0; i < numWorkers; i++){
+    worker_shifts[i] = 0;
+   }
+   // map between day number and the number of shifts on that day
+   unordered_map<int, int> day_shifts;
+   for(int i = 0; i < avail.size(); i++){
+    day_shifts[i] = 0;
+   }
     // Add your code below
+   return scheduleHelper(avail, dailyNeed, maxShifts, sched, 0, 0, worker_shifts, day_shifts);
+    
+}
+
+bool scheduleHelper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    int rowIdx,
+    int colIdx, 
+     unordered_map<int,int>& worker_shifts,
+     unordered_map<int,int>& day_shifts){
 
 
+  // if full return true 
+  if(rowIdx == avail.size()){
+    
+    return true;
+  }
 
+  // number of workers to check (how many different workers there are)
+  unsigned int numWorkers = avail[0].size();
+  // iterate through all these workers
+  for(unsigned int i =0; i < numWorkers; i++){
+    
+    // worker not available so don't even bother trying them
+    if(avail[rowIdx][i] == 0){
+        continue;
+    }
+    // worker already max scheduled so also don't bother
+    // if(worker_shifts[i] > maxShifts){
+    //      continue;
+    //  }
+    // //  skip because max reached for daily need
+    //  if(day_shifts[rowIdx] >= dailyNeed){
+    //      continue;
+    //  }
+    
+    // add worker to the schedule
+    sched[rowIdx].push_back(i);
+    worker_shifts[i] += 1;
+    day_shifts[rowIdx] += 1;
+    // check if this was a valid addition
+    if(isValid(rowIdx, colIdx, maxShifts, sched, dailyNeed, worker_shifts, day_shifts)){
+      // move on to next row
+      int nextRow, nextCol;
+      // determine next parameters
+      if(colIdx == dailyNeed - 1){
+        nextRow = rowIdx + 1;
+        nextCol = 0;
+      } else {
+        nextCol = colIdx + 1; 
+        nextRow = rowIdx;
+      }
+      // recurse
+      bool res = scheduleHelper(avail, dailyNeed, maxShifts, sched, nextRow, nextCol, worker_shifts, day_shifts);
+      if(res){
+        return true;
+      }
+    }
+    // remove worker from schedule and try next worker
+    sched[rowIdx].pop_back();
+    worker_shifts[i] -= 1;
+    day_shifts[rowIdx] -= 1;
+  }
+  return false;
+}
+
+// check if adding worker id to schedule is valid 
+// based on maxShifts, dailyNeed and availability
+bool isValid(int row, int schedCol, const size_t maxShifts, DailySchedule& sched, const size_t dailyNeed, unordered_map<int,int>& worker_shifts, unordered_map<int,int>& day_shifts){
+  
+   // figure out worker's id number
+  int worker_id = sched[row][schedCol];
+  // extract that worker from the map
+  int numShifts = worker_shifts[worker_id];
+  // determine if that worker is scheduled too many times
+  if(numShifts > maxShifts){
+    return false;
+  }
+  // determine if there are too many shifts on that day
+  if(day_shifts[row] > dailyNeed){
+    return false;
+  }
+  
+  // all conditions passed, return true
+  return true;
 
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -3,17 +3,18 @@
 #include <iostream>
 // For std::remove
 #include <algorithm> 
-#include <map>
 #include <set>
+#include <string>
 #endif
 
 #include "wordle.h"
 #include "dict-eng.h"
+
 using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void floatingSet(const string& in, multiset<char>& floaters, string curr, int numDashes, int numFloaters, int index, set<string>& resultSet, const std::set<std::string>& dict);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -21,8 +22,88 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
+    
+    //initialize string of dashes (empty)
+    std::string current(in.length(), '-');
+    // convert floating letters to set of chars
+    multiset<char> floaters;
+    for (char letter : floating) {
+        floaters.insert(letter);
+    }
+    // determine number of blank spaces in input string
+    int numDashes = 0;
+    for(int i = 0; i < in.length(); i++){
+      if(in[i] == '-'){
+        numDashes++;
+      }
+    }
+    // initialize empty results set
+    std::set<std::string> resultSet;
+
+    //run helper function
+    floatingSet(in, floaters, current, 0, numDashes, floaters.size(), resultSet, dict);
+    
+    // return final result set of strings
+    return resultSet;
 
 }
 
-// Define any helper functions here
+// generating all partially filled string combos using given floating letters
+void floatingSet(const string& in, multiset<char>& floaters, string curr, int index, int numDashes, int numFloaters, set<string>& resultSet, const std::set<std::string>& dict) {
+    // if end of word reached
+    if (index == in.length()) {
+        // make sure word is in the dictionary
+        if(dict.find(curr) == dict.end()){
+            return;
+        }
+        // add word to result set
+        resultSet.insert(curr);
+        return;
+    }
+    // letter found, not blank space
+    if(in[index] != '-'){
+      curr[index] = in[index];
+      // recurse
+      floatingSet(in, floaters, curr, index + 1, numDashes, numFloaters, resultSet, dict);
+    }
+    // blank space found 
+    else {
+      // if more floaters than blank space, impossible case
+      if (numFloaters > numDashes){
+        return;
+      }
+      // if floaters will fill in all the blank space
+      if(numFloaters == numDashes){
+        // iterate through all the floaters only testing them
+        multiset<char>::iterator it;
+        for(it = floaters.begin(); it != floaters.end(); ++it){
+          curr[index] = *it;
+          floaters.erase(it);
+          floatingSet(in, floaters, curr, index + 1, numDashes - 1, numFloaters - 1, resultSet, dict);
+          // reinsert
+          floaters.insert(*it);
+
+        } 
+      }
+        // number of floaters less than the number of blank space 
+        else if (numFloaters < numDashes){
+          //iterate through floaters first
+          multiset<char>::iterator it2;
+          for(it2 = floaters.begin(); it2 != floaters.end(); ++it2){
+            curr[index] = *it2;
+            floaters.erase(it2);
+            floatingSet(in, floaters, curr, index + 1, numDashes - 1, numFloaters - 1, resultSet, dict);
+            floaters.insert(*it2);
+          }
+          // iterate through letters of alphabet
+          for(int i = 97; i < 123; i++){
+            curr[index] = char(i);
+            floatingSet(in, floaters, curr, index + 1, numDashes - 1, numFloaters, resultSet, dict);
+          }
+        }
+      }
+}
+
+
+
+
